f4077b2f9ed36acb50d43299defbdda49ce72a97
Bryan Dixon <javawolfpack@gmail.com>
2019-01-29 15:14:21 -0800

    virtualenvs/python

---
+++ test.py
@@ -0,0 +1,16 @@
+print("Hello World")
+
+x=5
+x=5.1
+x="hello"
+def x(x):
+    print(x)
+x(str(x))
+int=5
+
+print("hello " + str(x))
+y="5"
+print(type(type))
+print(type(int))
+if type(x) == int:
+    print(type(x))
