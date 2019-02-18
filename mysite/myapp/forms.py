8b0b0dbaef49ef0e6067d3fde42293928745ee59
Bryan Dixon <javawolfpack@gmail.com>
2019-02-17 17:09:30 -0800

    simple django form

---
+++ mysite/myapp/forms.py
@@ -0,0 +1,12 @@
+from django import forms
+from django.core.validators import validate_slug
+
+def must_be_caps(value):
+    if not value.isupper():
+        raise forms.ValidationError("Not all uppercase")
+    # Always return the cleaned data, whether you have changed it or
+    # not.
+    return value
+
+class SuggestionForm(forms.Form):
+    suggestion_field = forms.CharField(validators=[must_be_caps],label='Suggestion', max_length=240)
--- mysite/myapp/views.py
+++ mysite/myapp/views.py
@@ -1,18 +1,38 @@
 from django.shortcuts import render
+from django.utils.html import escape
+
 # from django.http import HttpResponse
 # Create your views here.
 
 from . import models
+from . import forms
 
 def index(request):
+    #Form Submission
+    if request.method == "POST":
+        form_instance = forms.SuggestionForm(request.POST)
+        if form_instance.is_valid():
+            print(request.POST)
+            message = escape(form_instance.cleaned_data['suggestion_field'])
+            new_sugg = models.Suggestion()
+            new_sugg.suggestion_field = form_instance.cleaned_data["suggestion_field"]
+            new_sugg.save()
+            form_instance = forms.SuggestionForm()
+    else:
+        form_instance = forms.SuggestionForm()
+    #initial page load
+    if request.method == "GET":
+        print("GET")
     i_list = models.Suggestion.objects.all()
     n_list = []
-    for item in i_list:
-        n_list += [item.suggestion_field + "2"]
+
+    # for item in i_list:
+    #     n_list += [item.suggestion_field + "2"]
     context = {
         "body":"Hello World Template Variable",
         "title":"Title Hello",
-        "item_list":n_list
+        "item_list":i_list,
+        "form":form_instance
     }
     return render(request, "page.html", context=context)
 
